9b8fb8979ed4b47e2aa8e85bd85942b57a774e2f
Gage Sorrell <gage@sorrell.sh>
2024-09-14 12:29:57 -0400

    Temp to fix Daemon not being committed.

---
+++ Application/DaemonTemp/windows/Daemon/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Daemon::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Daemon::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
